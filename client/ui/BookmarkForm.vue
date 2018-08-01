1198ede6b1a40ac5056e92d24f185317432ebe9f
Kevin Lin <kevin.lin.p@gmail.com>
2018-08-01 09:23:39 -0700

    starting BookmarkForm

---
+++ client/ui/BookmarkForm.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  form
+    input.form-control(type='text' v-model='name' placeholder='name')
+    input.form-control(type='text' v-model='url' placeholder='url')
+    input.form-control(type='text' v-model='iconUrl' placeholder='iconUrl')
+</template>
+
+<script>
+  import { Bookmarks } from '/imports/api/bookmarks.js';
+
+  export default {
+    data: function() {
+      return {
+        name: 'Reddit',
+        url: 'https://www.reddit.com',
+        iconUrl: 'https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png'
+      };
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+</style>
--- client/ui/Bookmarks.vue
+++ client/ui/Bookmarks.vue
@@ -3,10 +3,12 @@
     .row
       .col-3(v-for='bookmark in bookmarks')
         bookmark(:bookmark='bookmark' :key='bookmark.id')
+    .row
+      .col
+        bookmark-form
 </template>
 
 <script>
-//https://www.redditstatic.com/desktop2x/img/favicon/android-icon-192x192.png
   import { Bookmarks } from '/imports/api/bookmarks.js';
 
   export default {
