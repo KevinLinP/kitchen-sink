f746326caf9ae7d5c4faf093444f50a80ea778eb
Kevin Lin <kevin.lin.p@gmail.com>
2018-08-11 15:06:12 -0700

    starting LoginPage

--- client/ui/LoginPage.vue
+++ client/ui/LoginPage.vue
@@ -1,21 +1,16 @@
 <template lang="pug">
 div
   div(v-if='loggedIn')
-    .mb-5
-      .h3 Currently logged in
-    div
-      button.btn.btn-secondary.pull-right(@click='logout') logout
+    .h3 Currently logged in
+    button.btn.btn-secondary.pull-right(@click='logout') logout
+
   div(v-else)
-    .mb-3
-      .h3 Login
-    form.form(@submit='login')
-      .input-group
-        input(type='text' v-model='email' readonly=true)
+    .h3.mb-3 Login
+    form(@submit='login')
+      input(type='text' v-model='email' readonly=true)
+      input(type='password' v-model='password' placeholder='password')
 
-      .input-group
-        input(type='password' v-model='password' placeholder='password')
-
-      button(type="submit" class="btn btn-primary") login
+      input(type="submit" class="btn btn-primary" value="login")
 </template>
 
 <script>
