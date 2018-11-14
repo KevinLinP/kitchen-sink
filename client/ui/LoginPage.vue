eca5d921344272f1e90239625d9695832345717b
Kevin Lin <kevin.lin.p@gmail.com>
2018-11-14 09:20:45 -0800

    Filling out LoginPage

--- client/ui/LoginPage.vue
+++ client/ui/LoginPage.vue
@@ -1,10 +1,37 @@
 <template lang="pug">
-div
+  .row.mt-5
+    .col-4
+    .col-4
+      div(v-if='currentUser')
+        .col Logged in
+      div(v-else)
+        form(@submit.prevent='submit')
+          .mb-2
+            input.form-control(type='text' v-model='email' placeholder='email')
+          .mb-2
+            input.form-control(type='password' v-model='password' placeholder='password')
+          input.form-control(type='submit' value='login')
 </template>
 
 <script>
-  const component = {
-  };
+  import { Meteor } from 'meteor/meteor';
 
-  export default component;
+  export default {
+    data: function() {
+      return {
+        email: '',
+        password: '',
+      };
+    },
+    meteor: {
+      currentUser() {
+        return Meteor.user();
+      }
+    },
+    methods: {
+      submit: function(e) {
+        Meteor.loginWithPassword(this.email, this.password);
+      }
+    }
+  }
 </script>
